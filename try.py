1869a296809dacaa0ad8cb7137bf37735add74b5
Geek HiMan <himreal9@gmail.com>
2021-05-03 16:46:14 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,30 @@
+import json
+from flask import Flask,render_template,request,redirect,session
+
+app=Flask(__name__)
+app.secret_key='man'
+
+
+@app.route('/',methods=['POST','GET']) 
+def con():
+    if request.method=='POST':
+        if request.form.get("se"):
+           o = request.form['ser']
+           with open('new.json',"r") as f:
+                k=json.load(f)
+                
+           with open('new.json',"w") as f:
+                k[o]={'age':23,'no':55}
+                d=json.dumps(k)
+                f.write(d)
+                
+           with open('new.json',"r") as f:
+                k=json.load(f)
+                k=str(k)
+                session['k']=k
+           return redirect('/')
+    else:        
+        return render_template("temp.html")
+
+if __name__=='__main__':
+    app.run()
